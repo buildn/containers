98f0994002e525b95a5509c00b02dc20024cd49d
Debasish Nath <debasish.nath@gmail.com>
2019-10-13 02:03:50 -0400

    docker image for kafka confluent components

---
+++ kafka/start.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+cd /bin
+set -m
+sh zookeeper-server-start /etc/kafka/zookeeper.properties &
+sh kafka-server-start /etc/kafka/server.properties &
+sh schema-registry-start /etc/schema-registry/schema-registry.properties &
+sh control-center-start /etc/confluent-control-center/control-center.properties
+
+fg %1
+
