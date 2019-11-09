01a1017ebecd6d83d0a9a04cfb483972ebe022cf
Debasish Nath <debasish.nath@gmail.com>
2019-11-09 10:19:20 -0500

    Using control central minimal configuration

--- kafka/start.sh
+++ kafka/start.sh
@@ -5,7 +5,7 @@ set -m
 sh zookeeper-server-start /etc/kafka/zookeeper.properties &
 sh kafka-server-start /etc/kafka/server.properties &
 sh schema-registry-start /etc/schema-registry/schema-registry.properties &
-sh control-center-start /etc/confluent-control-center/control-center.properties
+sh control-center-start /etc/confluent-control-center/control-center-minimal.properties
 
 fg %1
 
